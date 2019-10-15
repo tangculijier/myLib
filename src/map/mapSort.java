2f741562f7eed446d05d2a1cf730ecb3bb393e96
tangculijier <327493498@qq.com>
2019-10-15 15:01:08 +0800

    Update mapSort.java

--- src/map/mapSort.java
+++ src/map/mapSort.java
@@ -9,52 +9,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
-
 public final class mapSort {
-	
-	
 	 /*
 	  * LinkedHashMap保存了记录的插入顺序，在用Iterator遍历LinkedHashMap时，先得到的记录肯定是先插入的。
 	  * 此处换为其他map无法达到效果	
 	  */
-	public static Map<String, Integer> sortMapByValue(Map<String, Integer> oriMap) 
-	{
+	public static Map<String, Integer> sortMapByValue(Map<String, Integer> oriMap) {
 		//LinkedHashMap 
 		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
-		if (oriMap != null && !oriMap.isEmpty())
-		{
+		if (oriMap != null && !oriMap.isEmpty()){
 			List<Map.Entry<String, Integer>> entryList = new ArrayList<Map.Entry<String, Integer>>(oriMap.entrySet());
 			Collections.sort(entryList,
-					new Comparator<Map.Entry<String, Integer>>()
-					{
+					new Comparator<Map.Entry<String, Integer>>(){
 						public int compare(Entry<String, Integer> entry1,
-								Entry<String, Integer> entry2)
-						{
-							
+								Entry<String, Integer> entry2){
 								//todo
 								return 0 ;
 						}
 					});
 			Iterator<Map.Entry<String, Integer>> iter = entryList.iterator();
 			Map.Entry<String, Integer> tmpEntry = null;
-			while (iter.hasNext())
-			{
+			while (iter.hasNext()) {
 				tmpEntry = iter.next();
 				sortedMap.put(tmpEntry.getKey(), tmpEntry.getValue());
 			}
 		}
-		
 		for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
-
 		    System.out.println("sortedMap Key = " + entry.getKey() + ", sortedMap Value = " + entry.getValue());
-
 		}
-		
-		
 		return sortedMap;
 	}
-	
 }
 
 
